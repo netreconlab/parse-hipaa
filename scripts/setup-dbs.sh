131b5f7a1b21aa54f1f31c88ede9fb42e8f65223
Corey <coreyearleon@icloud.com>
2020-03-20 11:09:49 -0400

    added ability for regular postgres tests

--- scripts/setup-dbs.sh
+++ scripts/setup-dbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  
+
 set -e
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
     CREATE USER ${PG_PARSE_USER} LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '${PG_PARSE_PASSWORD}';
@@ -8,5 +8,16 @@ psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" 
     CREATE EXTENSION postgis;
     CREATE EXTENSION postgis_topology;
 EOSQL
+
+if [ -n "$PARSE_TEST" ]; then
+
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+    CREATE DATABASE parse_server_postgres_adapter_test_database;
+    \c parse_server_postgres_adapter_test_database;
+    CREATE EXTENSION postgis;
+    CREATE EXTENSION postgis_topology;
+EOSQL
+
+fi
 
 exec "$@"
