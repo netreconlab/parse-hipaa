ae5123982f8e9ee9f07f94620002bfd825cb805d
Corey MacBook Pro <coreyearleon@icloud.com>
2020-06-21 11:16:51 -0400

    lock in trusty version as we don't care about updates for sut

--- scripts/wait-for-parse.sh
+++ scripts/wait-for-parse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 until curl --silent parse:1337/parse | grep -q 'unauthorized'; do
-  >&2 echo "Parse is unavailable - sleeping"
+  >&2 echo "parse-server is unavailable - sleeping"
   sleep 1
 done
 
-echo "ParseServer is ready, starting test..."
-
+echo "parse-server is ready, starting test..."
+sleep 5 #Give some time for parse-server to run CloudCode to see if it crashes server
 #exec "$@"
