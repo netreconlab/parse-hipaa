cd10f252a69e85f78aaa65a4450b00d3464948b1
Corey's iMac <coreyearleon@icloud.com>
2020-11-11 19:04:13 -0500

    switch to Clock in parse/index.js

--- parse/index.js
+++ parse/index.js
@@ -232,7 +232,7 @@ async function createIndexes(){
     await adapter.ensureIndex('Outcome', versionedSchema, ['entityId'], 'Outcome'+indexEntityIdPostfix, false)
     .catch(error => console.log(error));
     
-    await adapter.ensureUniqueness('KnowledgeVector', schema, ['uuid'])
+    await adapter.ensureUniqueness('Clock', schema, ['uuid'])
     .catch(error => console.log(error));
     
     //Because of way ParseCareKit handles this class, comment out these checks
