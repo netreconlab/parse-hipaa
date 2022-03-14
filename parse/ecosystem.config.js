0cde0559beffb9dc0ab14c1b27116eb172126576
Corey's iMac <coreyearleon@icloud.com>
2022-03-13 20:33:10 -0400

    update ignore watch

--- parse/ecosystem.config.js
+++ parse/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name        : "parse-hipaa",
     script      : "./index.js",
-    ignore_watch: ["logs", "node_modules"],
+    ignore_watch: ["logs", "node_modules", ".pm2"],
     watch       : true,
     merge_logs  : true,
     cwd         : "/parse-server",
